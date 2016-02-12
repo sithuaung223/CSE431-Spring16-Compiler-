86af06b21f5d7d0f2ff617d028e52757db82701d
Evan Schwartzman <evan.schwartzman@wustl.edu>
2016-02-12 15:16:23 -0600

    draft, parses correctly, but doesn't evaluate. didn't put in grammar, first and follow yet, have paper copy

--- Labs/2/src/lab2/RecursiveDescent.java
+++ Labs/2/src/lab2/RecursiveDescent.java
@@ -19,24 +19,138 @@ public class RecursiveDescent {
    public RecursiveDescent() {
       Scanner.init();
 	
-      //
-      // FIXME:  Eliminate the loop below and instead call
-      //   the method associated with your grammar's goal symbol
-      //
-      //  So, eliminate from here....
-      Symbol t = Scanner.peek();
-      while (t.sym != sym.EOF) {
-         Listing.get().EmitMessage("Next token will be " + t);
-         Scanner.advance();
-         Listing.get().EmitMessage("Next2 token will be " + t);
-         t = Scanner.peek();
-      }
-      //  ... to here
+      Program();
       
    }
   
+   void Program(){
+	   Symbol t = Scanner.peek();
+	   if (t.sym == sym.lparen){
+		   File();
+	   }else{
+		   oops("no" + " " + t + " " + "Program");
+	   }
+   }
    
+   void File(){
+	   Symbol t = Scanner.peek();
+	   if (t.sym == sym.lparen){
+		   Lists();
+	   }else{
+		   oops("no" + " " + t + " "+" File");
+	   }
+   }
 
+   void Lists(){
+	   Symbol t = Scanner.peek();
+	   if (t.sym == sym.lparen){
+		   List();
+		   Lists_Two();
+	   }else{
+		   oops("no" + " " + t+ " " + "Lists");
+	   }
+   }
+   
+   void Lists_Two(){
+	   Symbol t = Scanner.peek();
+	   if (t.sym == sym.lparen){
+		   List();
+		   Lists_Two();  
+	   }else if(t.sym == sym.EOF){ //lamda
+	   }else{
+		   oops("no" + " " + t+ " " + "Lists_Two");
+	   }
+   }
+   
+   void List(){
+	   Symbol t = Scanner.peek();
+	   if (t.sym == sym.lparen){
+		   Scanner.advance();
+		   Expression();
+		   t = Scanner.peek();
+		   if (t.sym == sym.rparen){
+			   Scanner.advance();
+		   }else{
+			   oops("no rparen" + " " + t+ " " + "List"); 
+		   }
+	   }else{
+		   oops("no" + " " + t+ " " + "List");
+	   }
+   }
+   
+   void Expression(){
+	   Symbol t = Scanner.peek();
+	   if (t.sym == sym.plus){
+		   Scanner.advance();
+		   Operand();
+		   Operand();
+	   }else if(t.sym == sym.minus){
+		   Scanner.advance();
+		   Operand();
+		   Operand();
+	   }else if(t.sym == sym.times){
+		   Scanner.advance();
+		   Operand();
+		   Operand();
+	   }else if(t.sym == sym.negate){
+		   Scanner.advance();
+		   Operand();
+	   }else if(t.sym == sym.sum){
+		   Scanner.advance();
+		   Operands();
+	   }else if(t.sym == sym.product){
+		   Scanner.advance();
+		   Operands();
+	   }else if(t.sym == sym.mean){
+		   Scanner.advance();
+		   Operands();
+	   }else{
+		   oops("no" + " " + t+ " " + "Expression");
+	   }
+   }
+   
+   void Operand(){
+	   Symbol t = Scanner.peek();
+	   if (t.sym == sym.number || t.sym == sym.lparen){
+		   Atom();
+	   }else{
+		   oops("no" + " " + t+ " " + "Operand");
+	   }
+   }
+   
+   void Operands(){
+	   Symbol t = Scanner.peek();
+	   if (t.sym == sym.number || t.sym == sym.lparen){
+		   Operand();
+		   Operands_Two();
+	   }else{
+		   oops("no" + " " + t+ " " + "Operands");
+	   }
+   }
+   
+   void Operands_Two(){
+	   Symbol t = Scanner.peek();
+	   if (t.sym == sym.number || t.sym == sym.lparen){
+		   Operand();
+		   Operands_Two();
+		   t = Scanner.peek();
+	   }else if(t.sym == sym.rparen){
+	   }else{
+		   oops("no" + " " + t+ " " + "Operands_Two");
+	   }
+   }
+   
+   void Atom(){
+	   Symbol t = Scanner.peek();
+	   if (t.sym == sym.lparen){
+		  List();
+	   }else if (t.sym == sym.number){
+		   Scanner.advance();
+   	   }else{
+		   oops("no" + " " + t+ " " + "Atom");
+	   } 
+   }
+   
    void oops(String s) {
       System.err.println("Error: " + s);
       System.exit(-1);
