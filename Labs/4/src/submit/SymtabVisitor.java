0a62595b12874e18b15063217088ff065f4931c8
Evan Schwartzman <evan.schwartzman@wustl.edu>
2016-04-11 00:58:47 -0500

    Fields, Methods changes so each Class has its own table. Documentation. Finished?

--- Labs/4/src/submit/SymtabVisitor.java
+++ Labs/4/src/submit/SymtabVisitor.java
@@ -4,7 +4,23 @@
  * Evan Schwartzman
  * Si Thu Aung
  * 
- * Used the course symbol table, course parser
+ * We used the course symbol table and the course parser
+ * 
+ * A few notes:
+ * Our implementation made a few different design decisions than the reference.
+ * 
+ * 1. We think these were bugs that the reference didn't do this, but ours throws an error if the same method or class is declared in the same scope
+ * 
+ * 2. So that we do not throw an error if two distinct classes have fields with the same name we put the field's class name in its
+ * 	  symInfo object so if a field is already in the table, we can check if it is also in the same class before we throw an error. 
+ *    Also to facilitate this, we use the fact that in the parser, with f being the field node, 
+ *    f.getParent().getParent().getParent().getParent().toString() will always equal the class node, because of how the parser is structured,
+ *    and also with this parser, the fourth word of f.getParent().getParent().getParent().getParent().toString() will always be the class name.
+ *    We use all of this information to do the comparisons when a field with the same name is declared in two different classes (shouldn't fail because each class is its own "table")
+ *    
+ *    We do something almost identical to handle methods in different classes with the same name...
+ *    
+ * 3. We also added a place to put the scope of a variable in our symbol object so that two local declarations with the same name but different scopes would not fail
  * 
  */
 
@@ -81,28 +97,32 @@ public class SymtabVisitor extends NodeVisitor {
 		   sti.err((AbstractNode) d, "Duplicate Class Declaration!");
 	   }else{
 		   AbstractNode n = (AbstractNode) d;
-		   symbolInfo info = new symbolInfo(n, null, d.getMods(), sti.getCurrentNestLevel());
+		   symbolInfo info = new symbolInfo(n, null, d.getMods(), sti.getCurrentNestLevel(), d.getName());
 		   sti.enter(d.getName(), info);
 		   visitChildren(n);
 	   }
    }
    
    /**
-    * 1. Check if f is in symbol table - fail if it is in there
+    * 1. Check if f is in symbol table - fail if it is in there (unless it is in there under a different class)
     * 2. Insert f into symbol table (append this)
     */
    public void visit(FieldDeclaring f) {
-	   if(sti.lookup("this." +f.getName()) != null){
-		   sti.err((AbstractNode) f, "Duplicate Field Declaration!");
+	   AbstractNode n = (AbstractNode) f;
+	   String[] classOfField = n.getParent().getParent().getParent().getParent().toString().split(" "); 
+	   
+	   String className = ((symbolInfo) sti.lookup(classOfField[3])).checkClass();
+	   
+	   if(sti.lookup("this." +f.getName()) != null && (className == ((symbolInfo) sti.lookup("this." +f.getName())).checkClass())){
+		   sti.err((AbstractNode) f, "Duplicate Field Declaration");
 	   }else{
-		   AbstractNode n = (AbstractNode) f;
-		   symbolInfo info = new symbolInfo(n, f.getType(), f.getMods(), sti.getCurrentNestLevel());
+		   symbolInfo info = new symbolInfo(n, f.getType(), f.getMods(), sti.getCurrentNestLevel(), className);
 		   sti.enter("this." +f.getName(), info);
 	   }
    }
    
    /**
-    * 1. Check if l is in symbol table - fail if it is in there
+    * 1. Check if l is in symbol table - fail if it is in there (unless it is on different scope level)
     * 2. Insert l into symbol table
     * 3. Decorate Tree
     */
@@ -111,14 +131,14 @@ public class SymtabVisitor extends NodeVisitor {
 		   sti.err((AbstractNode) l, "Duplicate Local Declaration");
 	   }else{
 		   AbstractNode n = (AbstractNode) l;
-		   symbolInfo info = new symbolInfo(n, l.getType(), null, sti.getCurrentNestLevel());
+		   symbolInfo info = new symbolInfo(n, l.getType(), null, sti.getCurrentNestLevel(), null);
 		   sti.enter(l.getName(), info);
 		   l.setSymInfo(info);
 	   }
    }
    
    /**
-    * 1. Check if r is in symbol table - fail if it is not in there
+    * 1. Check if r is in symbol table, also check it with this. + r - fail if it is not in there
     * 2. Decorate Tree
     */
    public void visit(LocalReferencing r) {
@@ -138,18 +158,22 @@ public class SymtabVisitor extends NodeVisitor {
    }
    
    /**
-    * 1. Check if m is in symbol table - fail if it is in there
+    * 1. Check if m is in symbol table - fail if it is in there (unless in different class)
     * 2. Insert m into symbol table
     * 3. Pass visitor on to parameters (scope should be incremented by 1)
-    * 4. Pass visitor on to method body (treat as a block concerning scope)
+    * 4. Pass visitor on to method body (concerning scope, treat as a block)
     */
    public void visit(MethodDeclaring m) {
-	   if(sti.lookup(m.getName()) != null){
+	   AbstractNode n = (AbstractNode) m;
+	   String[] classOfMethod = n.getParent().getParent().getParent().getParent().toString().split(" ");
+	   
+	   String className = ((symbolInfo) sti.lookup(classOfMethod[3])).checkClass();
+	   
+	   if(sti.lookup(m.getName()) != null && (className == ((symbolInfo) sti.lookup(m.getName())).checkClass())){
 		   sti.err((AbstractNode) m, "Duplicate Method Declaration");
 	   }else{
-		   AbstractNode n = (AbstractNode) m;
-		   symbolInfo tv = new symbolInfo(n, m.getType(), null, sti.getCurrentNestLevel());
-		   sti.enter(m.getName(), tv);
+		   symbolInfo info = new symbolInfo(n, m.getType(), null, sti.getCurrentNestLevel(), className);
+		   sti.enter(m.getName(), info);
 		   
 		   sti.incrNestLevel();
 		   visitChildren(m.getParams());
@@ -166,21 +190,24 @@ public class SymtabVisitor extends NodeVisitor {
     * 
     * symbolInfo implements the SymInfo object
     * 
-    * added a getScopeLevel method to return current scope
+    * added a getScopeLevel method to make sure local declarations with the same name are accepted if on different scope level...
     * added a toString method
+    * added a checkClass method to make sure field, method declarations with the same name are accepted if in different classes...
     *
     */
    class symbolInfo implements SymInfo{
-		   public AbstractNode node;
-		   public TypeAttrs type;
-		   public ModsAttrs mods;
-		   public int scope;
-		   
-		public symbolInfo(AbstractNode n, TypeAttrs t, ModsAttrs m, int s ){
+	   public AbstractNode node;
+	   public TypeAttrs type;
+	   public ModsAttrs mods;
+	   public int scope;
+	   public String className;
+	   
+		public symbolInfo(AbstractNode n, TypeAttrs t, ModsAttrs m, int s, String c){
 			this.node = n;
 			this.type = t;
 			this.mods = m;
 			this.scope = s;
+			this.className = c;
 		}
 		@Override
 		public AbstractNode getDefiningNode() {
@@ -198,7 +225,7 @@ public class SymtabVisitor extends NodeVisitor {
 		}
 	
 		/*
-		 * Just stubs for now
+		 * Registers are stubs for now - hard code -1 just to be consistent at this point...
 		 */
 		@Override
 		public int getRegister() {
@@ -216,15 +243,19 @@ public class SymtabVisitor extends NodeVisitor {
 		public int getScopeLevel(){
 			return scope;
 		}
+		
+		public String checkClass(){
+			return className;
+		}
 	   
    }
    
    /** OPTIONAL (or from Studio if you want): Your symbol table implementation */
    class BuildSymtab extends Symtab implements SymtabInterface {
 
-	      public void incrNestLevel() { out("Nest level now " + getCurrentNestLevel()); }
-	      public void decrNestLevel() { out("Nest level now " + getCurrentNestLevel()); }
-	      public int getCurrentNestLevel() { return 0; }
-	      public SymInfo lookup(String id) { return  null; }
-	      public void enter(String id, SymInfo s) { }
-	   }
+	   public void incrNestLevel() { out("Nest level now " + getCurrentNestLevel()); }
+	   public void decrNestLevel() { out("Nest level now " + getCurrentNestLevel()); }
+	   public int getCurrentNestLevel() { return 0; }
+	   public SymInfo lookup(String id) { return  null; }
+	   public void enter(String id, SymInfo s) { }
+   }
